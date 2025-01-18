e0b38930bf1b12cf074254d0ead8e2b81d7d659c
se-opiko <opiopi206st@gmail.com>
2025-01-18 18:25:56 +0900

    add:front

---
+++ front/nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  // css
+  css: ['~/assets/scss/index.scss'],
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@element-plus/nuxt',
+    '@nuxtjs/color-mode'
+  ],
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // colorMode
+  colorMode: {
+    classSuffix: '',
+  },
+
+  unocss: {
+    uno: true,
+    attributify: true,
+    icons: {
+      scale: 1.2,
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
+        },
+      },
+    },
+  },
+  elementPlus: {
+    icon: 'ElIcon',
+    importStyle: 'scss',
+    themes: ['dark'],
+  },
+})
---
+++ front/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>サンプル</h4>
+  <el-button>ボタン</el-button>
+</template>
