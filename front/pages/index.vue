112b6ef6fb58082b62291a0da83536e54582ed2a
se-opiko <opiopi206st@gmail.com>
2025-04-26 13:13:21 +0900

    feat:TODOアプリの導線追加

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -18,7 +18,7 @@
             </el-card>
           </el-col>
           <el-col :span="6">
-            <el-card shadow="hover">
+            <el-card shadow="hover" @click="$router.push('/todo')">
               <el-image>
                 <template #error>
                   <div class="image-slot">
---
+++ front/pages/todo/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-container class="base h-100">
+    <!-- ヘッダー -->
+    <el-header class="p-2">
+      <el-page-header :icon="ArrowLeft" color="black" title="トップに戻る" @back="$router.back()">
+        <template #content>
+          <span class="text-large font-600 mr-3 text-black"> タスク管理 </span>
+        </template>
+      </el-page-header>
+    </el-header>
+    <!-- メイン -->
+    <el-main>
+      <!-- TOOD:検索エリアを作成する -->
+      <el-form>
+        <el-form-item>
+          <el-input v-model="searchWord" :prefix-icon="Search" placeholder="タスクを検索"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- FIXME:リアルタイム検索にする？ -->
+          <el-button type="primary" @click="onSubmit">検索</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- TODO:タブを追加する -->
+       <el-tabs v-model="activeTab">
+          <el-tab-pane label="すべて" name="all">
+            <template v-for="task in tasks" :key="task.id">
+              <el-card>
+                <div>
+                  <h3>{{ task.title }}</h3>
+                  <p>{{ task.description }}</p>
+                  <!-- 優先度 -->
+                  <span>{{ task.priority }}</span>
+                  <!-- 更新日 -->
+                  <span>{{ task.updatedAt }}</span>
+                </div>
+                <div>
+                  <el-button type="primary" icon="Edit" circle />
+                  <el-button type="danger" icon="Delete" circle />
+                </div>
+              </el-card>
+            </template>
+          </el-tab-pane>
+          <el-tab-pane label="進行中" name="progress"></el-tab-pane>
+          <el-tab-pane label="完了" name="complete"></el-tab-pane>
+       </el-tabs>
+      <!-- TODO:カードのリストを追加する -->
+    </el-main>
+  </el-container>
+</template>
+<script setup lang="ts">
+  import { ArrowLeft, Search, Edit, Delete } from '@element-plus/icons-vue'
+
+  const searchWord = ref('');
+  const activeTab = ref('all');
+  type Task = {
+    id: number;
+    title: string;
+    description: string;
+    priority: number;
+    updatedAt: string;
+  }
+
+  function onSubmit() {
+    
+  }
+</script>
+<style lang="css">
+  .base {
+    background-color: white;
+  }
+</style>
