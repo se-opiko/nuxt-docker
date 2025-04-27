e438d30d0a1e71957df1476adb7d45ec635cd5f6
se-opiko <opiopi206st@gmail.com>
2025-04-27 11:44:15 +0900

    feat:tasksテーブルを作成

---
+++ api/database/migrations/2025_04_27_022139_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description', 1000)->nullable();
+            $table->smallInteger('priority')->default(1);
+            $table->smallInteger('status')->default(1);
+            // created_at and updated_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
